6bb664980b8a6550fd22f3d88fb87c9a1ba4b44b
NewUserXml <115657694+NewUserXml@users.noreply.github.com>
2022-10-13 22:24:44 +0530

    Update v1.java

--- v1.java
+++ v1.java
@@ -14,5 +14,27 @@ class Solution {
             data.add(p);
             return data;
         }
+        int digit = up.charAt(0)-'0';
+        int i = (digit-2)*3;
+        //example: digit =2
+        // 2
+        //a  b  c
+          // 2  3  4
+    //start loop from 2 till 5
+        if (digit>7){
+            i++;
+        }
+        int len=i+3;
+        if (digit==7 || digit==9){
+            len++;
+        }
+        ArrayList<String>list = new ArrayList<>();
+        for (; i <len; i++) {
+            char ch = (char)('a'+i);
+            list.addAll(helper(p+ch, up.substring(1)));
+        }
+        return list;
+    }
+}s
 
         
