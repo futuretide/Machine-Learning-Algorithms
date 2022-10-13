5499a0010e426e875a52229879d40ac4d82e47d5
NewUserXml <115657694+NewUserXml@users.noreply.github.com>
2022-10-13 22:41:58 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -5,6 +5,8 @@ def threeSumClosest(self, nums: List[int], target: int) -> int:
             S1 = nums[i] + nums[i+1] + nums[i+2]
             S2 = nums[i] + nums[n-2] + nums[n-1]
             candidates = [result, S1, S2]
+            cand = 3;
+        #this is the three sum closest 
             result = min(candidates, key = lambda x: abs(x-target))
             if S1 <= target <= S2:
                 left, right = i+1, n-1
